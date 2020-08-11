72e60e016fd7ebc6a784794b3b2fb1db1e1c1f1d
Hamza <hamza_dar1@live.com>
2020-08-11 17:51:19 +0500

    User Profile

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterFrom
 # Create your views here.
 
@@ -15,3 +16,9 @@ def register(request):
     else:
         form = UserRegisterFrom()
     return render(request, 'users/register.html', {'form': form})
+
+
+# decorator
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
