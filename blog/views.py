4c3fd14d73d429ae7aedd724b0992ec2b3175d31
Hamza <hamza_dar1@live.com>
2020-08-11 15:54:21 +0500

    User Registration App

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Dar',
-        'title': 'Post 1',
-        'content': 'Lots of usless text',
-        'date_posted': 'August, 6, 2020'
-    },
-    {
-        'author': 'Binod',
-        'title': 'Post 2',
-        'content': 'More useless text but by Binod',
-        'date_posted': 'August, 7, 2020'
-    }
-]
+# dummy data
+# posts = [
+#     {
+#         'author': 'Dar',
+#         'title': 'Post 1',
+#         'content': 'Lots of usless text',
+#         'date_posted': 'August, 5, 2020'
+#     },
+#     {
+#         'author': 'Binod',
+#         'title': 'Post 2',
+#         'content': 'More useless text but by Binod',
+#         'date_posted': 'August, 7, 2020'
+#     }
+# ]
 
 def home(request):
     context= {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterFrom
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {username}')
+            return redirect('Blog-Home')
+    else:
+        form = UserRegisterFrom()
+    return render(request, 'users/register.html', {'form': form})
