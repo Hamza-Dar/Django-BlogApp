3c7bfbe4dc86a005665e6a55893e09ac8f224c42
Hamza <hamza_dar1@live.com>
2020-08-12 18:50:32 +0500

    User Profile Update v1

--- project/urls.py
+++ project/urls.py
@@ -25,10 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('register/',user_views.register, name='register'),
     path('', include('blog.urls')),
-    path('profile/', user_views.profile, name='profile'),
-
+    path('', include('users.urls')),
+    # path('profile/', user_views.profile, name='profile'),
+    # path('register/',user_views.register, name='register'),
 ]
 
 # only recommended in development/debugging
--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+import os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+    def save(self):
+        super().save()
+
+        image = Image.open(self.img.path)
+        if image.height > 300 or image.width > 300:
+            output_size= (300,300)
+            image.thumbnail(output_size)
+            image.save(self.img.path)
